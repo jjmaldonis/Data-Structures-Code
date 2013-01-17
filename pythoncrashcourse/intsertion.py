3fdfb41649febfce27a7e92119b6714b49f35ad2
Jason Maldonis <jjmaldonis@gmail.com>
2013-01-16 21:21:44 -0600

    updating insertion sort file again x2

--- pythoncrashcourse/intsertion.py
+++ pythoncrashcourse/intsertion.py
@@ -1,8 +1,19 @@
-#insertion -- impletments insertion sort
+#if you import this module you can type help(insertion) and it will give you the help info below
+"""
+insertion sort
+
+script that demonstrates implementation of insertion sort.
+"""
 
 import random
 
 def insertion(l):
+    """
+    implements insertion sort
+
+    pass it a list, list will be modified in plase
+    """
+
     print ("in insertion function")
     for i in range(1,len(l)):
         for j in range(0,i):
@@ -20,7 +31,7 @@ def main():
     #l = [9,7,4,2]
     print( "Before Sort: {0}".format(l) )
 
-    l = insertion(l)
+    insertion(l) #you don't need l =
     print( "After Sort: {0}".format(l) )
 
 if __name__ == "__main__":
