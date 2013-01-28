b9c5cc00850654c495c631909f2625b446cc7fb0
Jason Maldonis <jjmaldonis@gmail.com>
2013-01-28 09:52:56 -0600

    update and adding a matrix

--- p02/solver.py
+++ p02/solver.py
@@ -3,7 +3,7 @@ import getopt
 import hashtable
 
 def search(word,dic): #searches dic for word and for word.reverse()
-    print(word)
+    #print(word)
     found = dic.find(word)
     if found != -1:
         print("I found the word '" + word + "'!")
@@ -56,7 +56,7 @@ def main():
             word = word + matrix[c][r] 
         for i in range(0,ncol-1): #ncol = len(word) so i use that instead
             for j in range(i+2,ncol+1):
-                print("{},{}".format(i,j))
+                #print("{},{}".format(i,j))
                 search(word[i:j],dic)
 
     for k in range(0,ncol): #these next 4 for loops are for the diagonal cases
@@ -64,37 +64,41 @@ def main():
         j = k
         i = 0
         while (j < ncol and i < nrows):
+            #print("{},{}".format(i,j))
             word = word + matrix[i][j]
             i = i + 1
             j = j + 1
-        search(word)
+        search(word,dic)
     for k in range(0,nrows):
         word = ''
         j = k
         i = 0
         while (j < nrows and i < ncol):
+            #print("{},{}".format(i,j))
             word = word + matrix[j][i]
             i = i + 1
             j = j + 1
-        search(word)
+        search(word,dic)
     for k in range (0,ncol):
         word = ''
         j = ncol - k - 1
         i = 0
         while (j > 0 and i < nrows-1):
+            #print("{},{}".format(i,j))
             word = word + matrix[j][i]
             i = i + 1
             j = j - 1
-        search(word)
+        search(word,dic)
     for k in range(0,nrows):
         word = ''
         j = nrows -k - 1
         i = 0
         while (j < nrows-1 and i < ncol):
+            #print("{},{}".format(i,j))
             word = word + matrix[j][i]
             i = i + 1
             j = j -1
-        search(word)
+        search(word,dic)
 
 
 
