eaf6251f6702f095960f5dddc7b155324979e78f
Jason Maldonis <jjmaldonis@gmail.com>
2013-02-08 09:54:26 -0600

    fixed comments

--- p01/quick.py
+++ p01/quick.py
@@ -4,17 +4,23 @@ import random
 def quick(list):
     if(len(list) <= 1):
         return list
-    pivot = -1 #set the pivot to the last element
+    #set the pivot to the last element
+    pivot = -1
     i = 0
-    for j in range(len(list)-1): #iterate through the entire list except the pivot (which is the last thing)
+    #iterate through the entire list except the pivot (which is the last thing)
+    for j in range(len(list)-1):
         if list[j] <= list[pivot]:
             list[i], list[j] = list[j], list[i] #swap
             i = i + 1
-    list[i], list[pivot] = list[pivot], list[i] #switch the pivot with i
+    #switch the pivot with i
+    list[i], list[pivot] = list[pivot], list[i] 
     #now the pivot is in the correct spot with everything to its left lower than it and everything to its right higher than it
-    list_left = quick(list[:i]) #sort the left side of the pivot
-    list_right = quick(list[i+1:]) #sort the right side of the pivot
-    return list_left + [list[i]] + list_right #return the sorted left side + pivot + sorted right side
+
+    #sort the left and sides of the pivot
+    list_left = quick(list[:i])
+    list_right = quick(list[i+1:])
+    #return the sorted left side + pivot + sorted right side
+    return list_left + [list[i]] + list_right
 
 def main():
     list = range(100)
