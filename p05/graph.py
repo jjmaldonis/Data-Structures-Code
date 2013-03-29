ce55f7d424bd2d804fea36b0cd94a6892fad8e75
Jason Maldonis <jjmaldonis@gmail.com>
2013-03-29 14:46:49 -0500

    adding project 5

---
+++ p05/graph.py
@@ -0,0 +1,10 @@
+
+
+graph = {};
+
+#Psuedo code for adding to the graph:
+#To add node 'A' with neighbors 'B','C' with lengths between them of 5 and 8 respectively:
+#   graph['A'] = [{'B':5},{'C':8}];
+
+
+
