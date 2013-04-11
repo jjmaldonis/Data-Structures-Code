688feac8954b536e7001f845a0ca63ea546d7510
Jason Maldonis <jjmaldonis@gmail.com>
2013-04-11 18:40:15 -0500

    fixing issues Fixes #4 Fixes #5 Fixes #6 Fixes #9

--- p02/hashtable.py
+++ p02/hashtable.py
@@ -1,23 +1,30 @@
-
+""" Hashtable class """
 
 class Hashtable:
+    """ re-implements python's hash table functionality """
     PERTURB_SHIFT = 5
 
     def __init__(self):
+        """ init function for my hash table. """
         self.pos = 0
-        self.size = 3 #this is the power of 2, not the actual size of the table
+        #The size below is the power of 2 of the hashtable, not the actual size of the table
+        self.size = 3
         self.htable = [None] * (2**self.size)
         self.entries = 0.0
-        self.lf = 0.0 #load factor
+        #Let the load factor to 0 initially
+        self.lf = 0.0
         return None
 
     def print_table(self):
-        for i in range(0,2**self.size):
+        """ Prints the hash table for the user (mainly for debugging) """
+        for i in range(0, 2**self.size):
             print( '  ' + self.htable[i] )
 
-    def c_mul(self,a,b):
+    def c_mul(self, a, b):
+        """ This is how python creates some crazy but unique numbers based on other numbers """
         return eval(hex((long(a) * b) & 0xFFFFFFFFL)[:-1])
-    def pos_str_elem(self,element):
+    def pos_str_elem(self, element):
+        """ This is how python evaluates a string into an int value for hash usage """
         value = ord(element[0]) << 7
         for char in element:
             value = self.c_mul(1000003, value) ^ ord(char)
@@ -27,68 +34,72 @@ class Hashtable:
         return value
 
     def rehash(self):
+        """ Rehashing function """
         print("Rehash table to size {}!".format(2**(self.size+1)))
         self.entries = 0.0
         self.pos = 0
         self.size = self.size + 1
-        temphtable = self.htable[:] #copy old table
-        self.htable = [None] * (2**self.size) #replace old table with new, bigger hash table
-        for i in range(0,2**(self.size-1)):
+        #Copy the old table
+        temphtable = self.htable[:]
+        #Replace old table with the new bigger one
+        self.htable = [None] * (2**self.size)
+        for i in range(0, 2**(self.size-1)):
             if(temphtable[i] != None):
                 self.insert(temphtable[i])
             
         self.lf = (self.entries) / (2**self.size)
-        #self.print_table()
-        #print("Rehashing complete.")
         return None
 
-    def reset_pos(self,element): #this is python's hashing function (see increment_pos() for collisions)
+    #The following two functions are an implementation of python's hashing function (see increment_pos() for collisions)
+    def reset_pos(self, element):
+        """ Set position to the beginning of where it should be for a given input element """
         self.pos = self.pos_str_elem(element) & (2**self.size-1)
 
-    def increment_pos(self,perturb):
-        self.pos = ((5*self.pos) + 1 + perturb) & (2**self.size-1) #the '& (2**self.size-1)' is the same as '^ (2**self.size)'
+    def increment_pos(self, perturb):
+        """ A collision occured to get the next position pos should be set to """
+        #Side note: '& (2**self.size-1)' is the same as '^ (2**self.size)'
+        self.pos = ((5*self.pos) + 1 + perturb) & (2**self.size-1)
         perturb = perturb >> self.PERTURB_SHIFT
-        #print(self.pos)
         return perturb
 
-    def insert(self,element):
+    def insert(self, element):
+        """ Insert an element """
         self.reset_pos(element)
         perturb = self.pos_str_elem(element)
-        while(self.htable[self.pos] != None): #navigate until we get to an open spot
+        #Navigate until we get to an open spot
+        while(self.htable[self.pos] != None):
             perturb = self.increment_pos(perturb)
-        self.htable[self.pos] = element #;print("{0} was cast to {1}".format(element,self.pos))
+        self.htable[self.pos] = element
         self.entries = self.entries + 1
-        self.lf = ( self.entries ) / (2**self.size) #;print("load factor == {0}".format(self.lf))
+        self.lf = ( self.entries ) / (2**self.size)
         if(self.lf >= 2.0/3.0):
             self.rehash()
         return None
 
-    def find(self,element):
+    def find(self, element):
+        """ find an element in the hashtable
+        Returns the position if it is found else returns -1
+        """
         self.reset_pos(element)
         perturb = self.pos_str_elem(element)
         while(self.htable[self.pos] != None):
-            if element == self.htable[self.pos]: #found it! return where it's at!
+            if element == self.htable[self.pos]:
+                #Found it! Return where it's at.
                 return self.pos
-            else: #didn't find it, check in the next spot we would have put it
-                #print(self.htable[self.pos])
-                perturb = self.increment_pos(perturb) #;print("didn't find {0}".format(element))
+            else:
+                #Didn't find it, check the next spot we would have put it in
+                perturb = self.increment_pos(perturb)
         return -1
 
 
 def main():
+    """ main """
     htable = Hashtable()
     htable.insert("jason")
-    #htable.insert("jason")
     htable.insert("don")
     htable.insert("kyle")
     htable.insert("chris")
     htable.insert("jim")
-    #htable.insert("lauri")
-    #htable.insert("amanda")
-    #htable.insert("joel")
-    #htable.insert("donna")
-    #htable.insert("june")
-    #htable.insert("joe")
 
     htable.print_table()
 
--- p02/solver.py
+++ p02/solver.py
@@ -1,3 +1,5 @@
+""" Implements a word search given an input word search text file and an optional dictionary (otherwise it will use a default dictionary). """
+
 import time
 import sys
 import getopt
@@ -5,7 +7,11 @@ import hashtable
 
 PERTURB_SHIFT = 5
 
-def search(word,dic): #searches dic for word and for word.reverse(). returns 0 if found forward, returns 1 if found in reverse
+
+def search(word, dic):
+    """ Searches dic for word and for word.reverse().
+    Returns 0 if found forward, returns 1 if found in reverse.
+    """
     found = dic.find(word)
     if found != -1:
         print(word)
@@ -17,81 +23,84 @@ def search(word,dic): #searches dic for word and for word.reverse(). returns 0 i
 
 
 def main():
-
+    """ main function reads in a matrix and performs the word search algorithm """
     start = time.time()
-
-    #read in file argument (which is the word matrix) and print the matrix as well
-    infile = open(sys.argv[1],'r')
+    #Read in file argument (which is the word matrix) and print the matrix as well.
+    infile = open(sys.argv[1], 'r')
     matrix = []
     line = infile.readline()
-    if (line == ''): #if the input word matrix was blank
+    if (line == ''):
+        #If the input word maxtrix was blank
         return None
     while line != '':
-        line = line.replace(" ","")
-        matrix.append(line[:-1]) #dont include the eol char
-        #print(line)
+        line = line.replace(" ", "")
+        #Don't include the eol character in the line when we put it into the matrix
+        matrix.append(line[:-1])
         line = infile.readline()
     nrows = len(matrix[0])
     ncol = len(matrix)
-    #print("Matrix is {} by {}".format(nrows,ncol))
     time.sleep(.1)
     infile.close()
 
-    #load the dictionary into the hash table
-    #print("Loading the dictionary...")
-    dic = hashtable.Hashtable() #create hash table
+    #Load the dictionary into the hash table
+    dic = hashtable.Hashtable()
     if len(sys.argv) > 2:
-        infile = open(sys.argv[2],'r')
+        infile = open(sys.argv[2], 'r')
     else:
-        infile = open('/usr/share/dict/american-english-insane','r')
+        infile = open('/usr/share/dict/american-english-insane', 'r')
     line = infile.readline()
     while line != '':
         dic.insert(line[:-1])
         line = infile.readline()
     infile.close()
 
-    #search the matrix for words in the dictionary
-    for c in range(0,ncol):
-        for r in range(0,nrows):
-            #these next 8 variables are the row and column that we are at for each direction we could search (horizontal, vertical, diagonal left to right, and diagonal right to left)
-            h_c = c
-            h_r = r
-            v_c = c
-            v_r = r
-            dlr_c = c
-            dlr_r = r
-            drl_c = c
-            drl_r = r
-            #these are the words that will be searched for / are added to as we increment through the word matrix
+    #Search the matrix for words in the dictionary
+    deltaHoriz = [0,1]
+    deltaVert = [1,0]
+    deltaDiagLtoR = [1,1]
+    deltaDiagRtoL = [1,-1]
+    for c in range(0, ncol):
+        for r in range(0, nrows):
+            #These next 8 variables are the row and column that we are at for each direction we could search (horizontal, vertical, diagonal left to right, and diagonal right to left)
+            horiz = [c,r]
+            vert = [c,r]
+            diagLtoR = [c,r]
+            diagRtoL = [c,r]
+            #These are the words that will be searched for / are added to as we increment through the word matrix
             h_word = matrix[c][r]
             v_word = matrix[c][r]
             dlr_word = matrix[c][r]
             drl_word = matrix[c][r]
-            for x in range(c, ncol):
-                for y in range(r,nrows):
-                    if (h_c + 0 < ncol and h_r +1 < nrows): #if the next horizontal spot is not out of range:
-                        h_c = h_c + 0 #move in the direction we should move for the horizontal word (0 down, 1 right)
-                        h_r = h_r + 1
-                        h_word = h_word + matrix[h_c][h_r] #add onto the word
-                        search(h_word,dic) #search for the word (this searches forward and back)
-                    if (v_c + 1 < ncol and v_r +0 < nrows): #if the next vertical spot is not out of range:
-                        v_c = v_c + 1
-                        v_r = v_r + 0
-                        v_word = v_word + matrix[v_c][v_r]
-                        search(v_word,dic)
-                    if (dlr_c + 1 < ncol and dlr_r +1 < nrows): #if next diagonal left to right spot is not out of range:
-                        dlr_c = dlr_c + 1
-                        dlr_r = dlr_r + 1
-                        dlr_word = dlr_word + matrix[dlr_c][dlr_r]
-                        search(dlr_word,dic)
-                    if (drl_c + 1 < ncol and drl_r -1 >= 0): #if the next diagonal right to left spot is not out of range:
-                        drl_c = drl_c  + 1
-                        drl_r = drl_r  - 1
-                        drl_word = drl_word + matrix[drl_c][drl_r]
-                        search(drl_word,dic)
+            for _ in range(c, ncol):
+                for _ in range(r, nrows):
+                    if (horiz[0] + 0 < ncol and horiz[1] + 1 < nrows):
+                        #If the next horizontal spot is not out of range then move in the direction we should move for the horizontal word (0 down, 1 right)
+                        horiz[0] = horiz[0] + deltaHoriz[0]
+                        horiz[1] = horiz[1] + deltaHoriz[1]
+                        #Add the character onto the word and then search for the new word (this searches forward and backward)
+                        h_word = h_word + matrix[horiz[0]][horiz[1]]
+                        search(h_word, dic)
+                    if (vert[0] + 1 < ncol and vert[1] + 0 < nrows):
+                        #If the next vertical spot is not out of range we do very similar things.
+                        vert[0] = vert[0] + deltaVert[0]
+                        vert[1] = vert[1] + deltaVert[1]
+                        v_word = v_word + matrix[vert[0]][vert[1]]
+                        search(v_word, dic)
+                    if (diagLtoR[0] + 1 < ncol and diagLtoR[1] + 1 < nrows):
+                        #If the next diagonal left to right spot is not out of range we do very similar things.
+                        diagLtoR[0] = diagLtoR[0] + deltaDiagLtoR[0]
+                        diagLtoR[1] = diagLtoR[1] + deltaDiagLtoR[1]
+                        dlr_word = dlr_word + matrix[diagLtoR[0]][diagLtoR[1]]
+                        search(dlr_word, dic)
+                    if (diagRtoL[0] + 1 < ncol and diagRtoL[1] - 1 >= 0):
+                        #If the next diagonal right to left sopt is not out of range we do very similar things.
+                        diagRtoL[0] = diagRtoL[0] + deltaDiagRtoL[0]
+                        diagRtoL[1] = diagRtoL[1] + deltaDiagRtoL[1]
+                        drl_word = drl_word + matrix[diagRtoL[0]][diagRtoL[1]]
+                        search(drl_word, dic)
 
     end = time.time()
-    print("Program took {0} seconds to run.".format(end-start))
+    print("Program took {0} seconds to run.".format(end - start))
     return None
 
 
